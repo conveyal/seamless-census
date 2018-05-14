5fb2cb06ed61bdcfe883cbf16d7651762f4d647c
ansons <ansonstewart@gmail.com>
2018-05-14 15:44:35 -0400

    update to python 3

--- downloadData.py
+++ downloadData.py
@@ -4,7 +4,7 @@
 # Or use special code 'ALL'
 
 from sys import argv
-from urllib import urlretrieve
+from urllib.request import urlretrieve
 import zipfile
 import os
 import os.path
@@ -77,14 +77,14 @@ states = [state.upper() for state in argv[2:]]
 
 if len(states) == 1 and states[0] == 'ALL':
     # download all states
-    print "Downloading all states"
+    print("Downloading all states")
     states = fipsCodes.keys()
 
 # check inputs
-invalidStates = [state for state in states if not fipsCodes.has_key(state)]
+invalidStates = [state for state in states if not state in fipsCodes]
 
 if len(invalidStates) > 0:
-    print "Did not recognize states %s" % ' '.join(invalidStates)
+    print ("Did not recognize states %s" % ' '.join(invalidStates))
 
 # make the directory structure
 os.makedirs(os.path.join(outDir, 'tiger'))
@@ -93,11 +93,12 @@ os.makedirs(os.path.join(outDir, 'workforce'))
 
 # download resiliently
 def retrieve(url, path):
-    for i in xrange(50):
+    for i in range(50):
         try:
+            print("download attempt {0}".format(i))
             urlretrieve(url, path)
         except:
-            print "error retrieving {0}, retrying".format(url)
+            print("error retrieving {0}, retrying".format(url))
             sleep(5)
         else:
             break
@@ -105,8 +106,8 @@ def retrieve(url, path):
 
 # download the states
 for state in states:
-    print 'processing %s' % state
-    print 'Downloading TIGER'
+    print('processing %s' % state)
+    print('Downloading TIGER')
     # get tiger
     fips = fipsCodes[state]
     zipout = os.path.join(outDir, "tiger", "{0}.zip".format(state))
@@ -119,15 +120,15 @@ for state in states:
             name = os.path.split(member.filename)[-1]
             dest = os.path.join(outDir, 'tiger', name)
             with zf.open(member) as stream:
-                with open(dest, 'w') as out:
+                with open(dest, 'wb') as out:
                     copyfileobj(stream, out)
 
     # we no longer need the zipfile
     os.remove(zipout)
 
-    print 'Done with TIGER'
+    print('Done with TIGER')
 
-    print 'Downloading LODES data'
+    print('Downloading LODES data')
 
     # figure out the year of the latest available data
     # Most states have 2014 data available
@@ -138,11 +139,11 @@ for state in states:
     if state == 'WY':
         year = 2013
     elif state == 'PR' or state == 'VI':
-        print '{0} does not have LODES data available'.format(state)
+        print('{0} does not have LODES data available'.format(state))
         year = 0
 
     if year:
-        print "Dowloading {0} LODES data for {1}".format(year, state)
+        print("Dowloading {0} LODES data for {1}".format(year, state))
 
         # get the rac file
         out = os.path.join(outDir, 'workforce', '{0}_{1}_rac.csv.gz'.format(state, year))
@@ -152,4 +153,4 @@ for state in states:
         out = os.path.join(outDir, 'jobs', '{0}_{1}_wac.csv.gz'.format(state, year))
         retrieve("http://lehd.ces.census.gov/data/lodes/LODES7/{0}/wac/{0}_wac_S000_JT00_{1}.csv.gz".format(state.lower(), year), out)
 
-    print 'Done with {0}'.format(state)
+    print('Done with {0}'.format(state))
